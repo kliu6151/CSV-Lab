fa6fb42dc468fd51992623f02dbab00b6f743311
BT_1N3_27@bktech.local <BT_1N3_27@bktech.local>
2018-12-13 10:15:32 -0500

    Initial commit

---
+++ src/CSVUtilities.java
@@ -0,0 +1,30 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CSVUtilities {
+
+    ArrayList<String> CSVData;
+    int numColumns;
+
+    CSVUtilities(File csv)
+    {
+
+    }
+    public List<String> getColumnHeaders()
+    {
+
+    }
+    public List<String> getDataString(int column)
+    {
+
+    }
+    public List<Integer> getDataInt(int column)
+    {
+
+    }
+    public List<Double> getDataDouble(int column)
+    {
+
+    }
+}
